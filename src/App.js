03284683b2945484ab9e1e01d670519170e137a9
HenriquePinheiro <pinheirohsp@gmail.com>
2022-07-13 18:12:39 -0300

    initial/final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa'
+import './app.css'
 
 function App() {
+  //viacep.com.br/ws/********/json/ 
+
+  const [cepInfo, setCepInfo] = useState({})
+
+  const fetchCEP = (cep) => 
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(res => res.json())
+        
+        
+  const handleCLick = async () => {
+    const json = await fetchCEP(input)
+    console.log(json.erro)
+
+    let { erro } = json
+
+    if(erro) return setCepInfo({ erro })
+    
+     let { logradouro, bairro, 
+      complemento, localidade, uf, cep} = json
+     
+     setCepInfo({
+       logradouro, 
+       bairro, 
+       complemento, 
+       localidade, 
+       uf,
+       cep,
+     })
+  }
+    
+  const [input, setInput] = useState('')
+  const handleChange = value => setInput(value) 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="main-title">Buscador CEP</h1>
+
+      <div className="container-input">
+          <input
+            onChange={e => handleChange(e.target.value)}
+            className='cep-input'
+            type="text"
+            value={input}
+            placeholder="Digite seu CEP"/>
+          
+          <button 
+            className="search-btn"
+            onClick={handleCLick}>
+              <FaSearch color='grey' />
+          </button>
+      </div>
+      {
+        Object.keys(cepInfo).length === 0 ? null :
+        Boolean('erro' in cepInfo) ? 
+        (<main><h2>CEP Not found!</h2></main>) : 
+        (<main>
+            <h2>CEP: {cepInfo.cep}</h2>
+            <ul className='cep-info-container'> 
+              {
+                Object.keys(cepInfo).map(key => {
+                  return (
+                    <li key={key} className='cep-info-item'>
+                        <span className='info-key'>
+                            {`${key}: `}
+                        </span>
+                        <span className='info-value'>
+                            {cepInfo[key]}
+                        </span>
+                    </li>      
+                  )
+                })
+              }
+            </ul>
+        </main>)
+      }
     </div>
   );
 }
